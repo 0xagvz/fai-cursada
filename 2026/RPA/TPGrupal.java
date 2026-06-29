e3cab048151b8b43653c521e2f903ade7e880353
Agus <101914474+0xagvz@users.noreply.github.com>
2026-06-29 14:47:07 -0300

    TpGrupalEntrega3

--- 2026/RPA/TPGrupal.java
+++ 2026/RPA/TPGrupal.java
@@ -8,30 +8,147 @@ public class TPGrupal {
     }
 
     public static void main(String[] args) {
-        Random rand = new Random();
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
 
-        System.out.println("Ingrese el lugar a patear");
-        int posicionPateador = sc.nextInt();
+        int golesJ1 = 0;
+        int golesJ2 = 0;
+        int posicionPateador1;
+        int posicionPateador2;
+        boolean hayGanador;
 
-        while (posicionPateador < 1 || posicionPateador > 3) {
-            System.out.println("Ingrese un numero entre 1 y 3");
-            posicionPateador = sc.nextInt();
-        }
+        //Tanda de 5 Penales
+         for (int i = 1; i <= 5; i++) { 
 
-        int posicionArquero = rand.nextInt(3) + 1;
+            System.out.println("RONDA " + i);
+            do {
+                System.out.println("Jugador 1, elije un lugar a patear (1-3)");
+                posicionPateador1 = sc.nextInt();
+                if (posicionPateador1 < 1 || posicionPateador1 > 3) {
+                    System.out.println("Posición inválida. Por favor, elige un número entre 1 y 3.");
+                } else {
+                    break;
+                }
+            } while (true);
 
-        System.out.println("Posicion del pateador: " + posicionPateador);
-        System.out.println("Posicion del arquero: " + posicionArquero);
+            int posicionArquero = rand.nextInt(3) + 1; 
 
-        boolean gol = esGol(posicionPateador, posicionArquero);
+            System.out.println("El jugador pateara en: " + posicionPateador1);
+            System.out.println("El arquero se tirara en: " + posicionArquero);
 
-        if (gol) {
-            System.out.println("Es Gol");
+            boolean gol = esGol(posicionPateador1, posicionArquero);
+
+            if (gol) {
+                System.out.println("Gol del jugador 1!!");
+                golesJ1++;
+            } else {
+                System.out.println("El arquero atajo el penal del jugador 1!!");
+            }
+
+            System.out.println("------------------------------------");
+
+            do {
+                System.out.println("Jugador 2, elije un lugar a patear (1-3)");
+                posicionPateador2 = sc.nextInt();
+                if (posicionPateador2 < 1 || posicionPateador2 > 3) {
+                    System.out.println("Posición inválida. Por favor, elige un número entre 1 y 3.");
+                } else {
+                    break;
+                }
+            } while (true);
+
+            posicionArquero = rand.nextInt(3) + 1;
+
+            System.out.println("El jugador pateara en: " + posicionPateador2);
+            System.out.println("El arquero se tirara en: " + posicionArquero);
+
+            gol = esGol(posicionPateador2, posicionArquero);
+
+            if (gol) {
+                System.out.println("Gol del jugador 2!!");
+                golesJ2++;
+            } else {
+                System.out.println("El arquero atajo el penal del jugador 2!!");
+            }
+         }
+        
+         //  Resultado después de 5 penales
+        System.out.println("Resultado final:");
+        System.out.println("Jugador 1: " + golesJ1 + " goles");
+        System.out.println("Jugador 2: " + golesJ2 + " goles");         
+
+         // Desempate 
+         if (golesJ1 == golesJ2) {
+            System.out.println("EMPATE! Vamos a muerte súbita...");
+
+            hayGanador = false;
+
+            do{
+                do {
+                    System.out.println("Jugador 1, elije un lugar a patear (1-3)");
+                    posicionPateador1 = sc.nextInt();
+                    if (posicionPateador1 < 1 || posicionPateador1 > 3) {
+                        System.out.println("Posición inválida. Por favor, elige un número entre 1 y 3.");
+                    } else {
+                        break;
+                    }
+                } while (true);
+
+                int posicionArquero = rand.nextInt(3) + 1; 
+
+                System.out.println("El jugador pateara en: " + posicionPateador1);
+                System.out.println("El arquero se tirara en: " + posicionArquero);
+
+                boolean golp1 = esGol(posicionPateador1, posicionArquero);
+
+                if (golp1) {
+                    System.out.println("Gol del jugador 1!!");
+                } else {
+                    System.out.println("El arquero atajo el penal del jugador 1!!");
+                }
+
+                System.out.println("------------------------------------");
+
+                do {
+                    System.out.println("Jugador 2, elije un lugar a patear (1-3)");
+                    posicionPateador2 = sc.nextInt();
+                    if (posicionPateador2 < 1 || posicionPateador2 > 3) {
+                        System.out.println("Posición inválida. Por favor, elige un número entre 1 y 3.");
+                    } else {
+                        break;
+                    }
+                } while (true);
+
+                posicionArquero = rand.nextInt(3) + 1;
+
+                System.out.println("El jugador pateara en: " + posicionPateador2);
+                System.out.println("El arquero se tirara en: " + posicionArquero);
+
+                boolean golp2 = esGol(posicionPateador2, posicionArquero);
+
+                if (golp2) {
+                    System.out.println("Gol del jugador 2!!");
+                } else {
+                    System.out.println("El arquero atajo el penal del jugador 2!!");
+                }
+                
+                if (golp1 && !golp2) {
+                    System.out.println("--- Jugador 1 gana en muerte súbita! ---");
+                    hayGanador = true;
+                } else if (!golp1 && golp2) {
+                    System.out.println("--- Jugador 2 gana en muerte súbita! ---");
+                    hayGanador = true;
+                } else {
+                    System.out.println("Siguen empatados, otra ronda...");
+                }
+
+            }while (!hayGanador);
+        } else if (golesJ1 > golesJ2) {
+            System.out.println("--- Gana el Jugador 1! ---");
         } else {
-            System.out.println("No Es Gol");
+            System.out.println("--- Gana el Jugador 2! ---");
         }
 
-        sc.close();
     }
+
 }
